6514ed1e7d25cfeae2bd71686eb5958263a54682
Chris Couchoud <couchoud@gmail.com>
2022-09-20 15:59:20 -0400

    simplified competitions list layout

--- src/views/competitions/Competitions.vue
+++ src/views/competitions/Competitions.vue
@@ -15,31 +15,27 @@
     <article class="article p-4 has-background-white-bis">
       <div 
         v-if="list"
-        class="columns is-multiline"
+        class="is-flex is-flex-wrap-wrap"
       >
-        <div 
+        <div  
           v-for="(comp, i) in list" 
           :key="i"
-          class="column is-one-third"
-        > 
-          <div
-            @click="$router.push({ name: 'comp-show', params: { id: comp.id } })" 
-            class="box"
-          >
-            <router-link :to="{ name: 'comp-show', params: { id: comp.id } }"> 
-              <h3 class="title is-3">{{ comp.name }}</h3>
-              <h4 class="subtitle is-4">$X,XXX in prizes to be won</h4>
-            </router-link>
-            <div class="duration mt-6 mb-4">
-              <p v-if="comp.start_date && comp.end_date" >
-                {{ kDayMonth(comp.start_date) }} - {{ kDayMonthYear(comp.end_date) }}
-              </p>
-              <p v-else>
-                Time TBD
-              </p>
-            </div>
-            <countdown-timer :date="comp.start_date" />
+          @click="$router.push({ name: 'comp-show', params: { id: comp.id } })" 
+          class="box mb-4 mr-4"
+        >
+          <router-link :to="{ name: 'comp-show', params: { id: comp.id } }"> 
+            <h3 class="title is-3">{{ comp.name }}</h3>
+            <h4 class="subtitle is-4">$X,XXX in prizes to be won</h4>
+          </router-link>
+          <div class="duration mt-6 mb-4">
+            <p v-if="comp.start_date && comp.end_date" >
+              {{ kDayMonth(comp.start_date) }} - {{ kDayMonthYear(comp.end_date) }}
+            </p>
+            <p v-else>
+              Time TBD
+            </p>
           </div>
+          <countdown-timer :date="comp.start_date" />
         </div>
       </div>
       <loading v-else />
