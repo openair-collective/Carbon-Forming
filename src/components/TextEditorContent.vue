8082728fffc1a12faed6ef2f49c6b9852516da08
Chris Couchoud <dev@southriverstudios.com>
2022-10-20 09:28:20 -0400

    [WIP] Added Quill editor, Text Editor components

---
+++ src/components/TextEditorContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-editor">
+    <div class="content" v-html="value" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
