68b7dca9ad6dc86963f9e3da37d8a0e9beed13ce
Chris Couchoud <dev@southriverstudios.com>
2022-10-21 15:31:55 -0400

    #21 > Added project image uploads

--- src/components/project/ProjectInput.vue
+++ src/components/project/ProjectInput.vue
@@ -7,6 +7,55 @@
       </div>
     </div>
     <hr/>
+    <div class="field"> 
+      <label class="label">Project Image</label>
+      <p
+        v-if="!project.image" 
+        class="mb-2"
+      >
+        Please upload and image for your project, we suggest a photo or drawing of your expected design. (Required)
+      </p>
+      <p 
+        v-if="!project.image"
+        class="help is-info mb-2"
+      >
+        Accepts .jpeg, .jpg, and .png files only. Maximum file size is {{ kImageMaxSize / 1000 }}kb. Dimensions (----px x ----px).
+      </p>
+      <div class="is-flex is-flex-direction-row">
+        <div
+          v-if="imageUrl !== kImagePlaceholder" 
+          class="project-image mr-4 has-border-radius-6 has-background-grey-lighter has-text-centered"
+          :style="{ 'background-image': 'url(' + imageUrl + ')' }"
+        >
+        </div>
+        <button
+          v-if="project.image"
+          @click.prevent="$emit('remove-image')"
+          class="button is-warning"
+        >
+          Remove Image
+        </button>
+        <div
+          v-else
+          class="file mb-2">
+          <label class="file-label">
+            <input
+              @change="onImageFileChange"
+              class="file-input" 
+              type="file"
+              accept="image/*"
+              ref="file_image"
+            >
+            <span class="file-cta">
+              <span class="file-label">
+                {{ imageUrl ===  kImagePlaceholder ?  'Upload Image' : 'Change Image' }}
+              </span>
+            </span>
+          </label>
+        </div>
+      </div>
+    </div>
+    <hr/>
     <h2 class="title is-4">Bill of Materials</h2>
       <p class="mb-2">Please list all of the materials used in the construction of your project.</p>
       <div>
@@ -41,7 +90,7 @@
               <td><input type="number" class="input" v-model="m.quantity" min="1" /></td>
               <td>
                 <input 
-                  type="url" 
+                  type="url"
                   class="input" 
                   placeholder="https://example.com" 
                   pattern="https?://.*"
@@ -74,12 +123,12 @@
       <label class="label">Design document link</label>
       <div class="control">
         <input
-          class="input" 
           type="url"
-          v-model="project.design_doc_url"
+          class="input" 
+          placeholder="https://example.com" 
           pattern="https?://.*"
           title="Must be a valid URL"
-          :disabled="!!docFile"
+          v-model="project.design_doc_url"
         >
       </div>
     </div>
@@ -102,9 +151,11 @@ import { defineComponent } from 'vue'
 import { Project, Material, FileUpload } from '@/types'
 import { mapStores } from 'pinia'
 import { useProjectsStore } from '@/store/projects'
+import { PROJECT_IMAGE_PLACEHOLDER } from '@/consts'
 import log from '@/services/logger'
 
 const MODULE_ID = 'components/project/ProjectInput'
+const IMAGE_MAX_FILE_SIZE = 400 * 1000 // 400kb
 
 function initMaterial():Material {
   return {
@@ -126,7 +177,10 @@ export default defineComponent({
     return {
       error: '',
       success: '',
-      docFile: undefined as File|undefined
+      docFile: undefined as File|undefined,
+      imagePreviewUrl: '',
+      kImageMaxSize: IMAGE_MAX_FILE_SIZE,
+      kImagePlaceholder: PROJECT_IMAGE_PLACEHOLDER
     }
   },
   computed: {
@@ -136,7 +190,21 @@ export default defineComponent({
         return !p.name || !p.quantity
       })
       return materialErrors.length === 0
-    }
+    },
+    hasValidImage():boolean {
+      let result = false
+      if (this.docFile) {
+        result = this.docFile.size < IMAGE_MAX_FILE_SIZE
+      }
+      return result
+    },
+    imageUrl():string { 
+      let result = this.imagePreviewUrl || PROJECT_IMAGE_PLACEHOLDER
+      if (this.project.image && this.project.image.url) {
+        result = this.project.image.url
+      }
+      return result
+    },
   },
   created() {
     if (!this.project.materials || !this.project.materials.length) {
@@ -154,7 +222,28 @@ export default defineComponent({
       else {
         this.project.materials.splice(idx, 1)
       }
+    },
+    onImageFileChange(event:Event) {
+      const files = (event.target as HTMLInputElement).files
+      this.docFile = files && files.length ? files[0] : undefined
+      this.showImagePreview()
+    },
+    showImagePreview (){
+      const image:HTMLInputElement = this.$refs.file_image as HTMLInputElement
+      const file: File = (image.files as FileList)[0]
+      this.imagePreviewUrl = file ? URL.createObjectURL(file) : PROJECT_IMAGE_PLACEHOLDER
     }
   }
 })
 </script>
+
+<style scoped>
+.project-image {
+  flex-grow: 1;
+  height: 200px;
+  max-width: 50%;
+  background-size: contain;
+  background-position: center ;
+  background-repeat: no-repeat;
+}
+</style>
