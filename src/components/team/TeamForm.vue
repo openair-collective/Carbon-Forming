fdcf32278db5c888cb092a05edfd94bad9039317
Chris Couchoud <dev@southriverstudios.com>
2022-12-06 11:56:00 -0500

    [WIP] Added Team deletion flow

--- src/components/team/TeamForm.vue
+++ src/components/team/TeamForm.vue
@@ -1,124 +1,141 @@
 <template>
-  <form @submit.prevent="submitTeamForm" :disabled="isSaving">
-    <div class="is-flex is-flex-direction-row mb-4">
-      <div>
-        <div class="image is-128x128 mr-4">
-          <img :src="teamImageUrl" />
+  <section class="section">
+    <form @submit.prevent="submitTeamForm" :disabled="isSaving">
+      <div class="is-flex is-flex-direction-row mb-4">
+        <div>
+          <div class="image is-128x128 mr-4">
+            <img :src="teamImageUrl" />
+          </div>
         </div>
-      </div>
-      <div>
-        <label class="label">Team Avatar</label>
-        <button
-            v-if="clone.avatar"
-            @click.prevent="removeAvatar"
-            class="button is-warning"
-          >
-            Remove Avatar
-        </button>
-        <div v-else>
-          <p class="help is-info mb-2">
-           Accepts .jpeg, .jpg, and .png files only. Maximum file size is {{ kAvatarMaxSize / 1000 }}kb.
-          </p>
-          <div class="file mb-2">
-            <label class="file-label">
-              <input 
-                @change="showAvatarPreview"
-                class="file-input" 
-                type="file"
-                accept="image/*"
-                ref="file_avatar"
-              >
-              <span class="file-cta">
-                <span class="file-label">
-                  {{ teamImageUrl === kAvatarPlaceholder  ?  'Upload Team Avatar' : 'Change Avatar' }}
+        <div>
+          <label class="label">Team Avatar</label>
+          <button
+              v-if="clone.avatar"
+              @click.prevent="removeAvatar"
+              class="button is-warning"
+            >
+              Remove Avatar
+          </button>
+          <div v-else>
+            <p class="help is-info mb-2">
+            Accepts .jpeg, .jpg, and .png files only. Maximum file size is {{ kAvatarMaxSize / 1000 }}kb.
+            </p>
+            <div class="file mb-2">
+              <label class="file-label">
+                <input 
+                  @change="showAvatarPreview"
+                  class="file-input" 
+                  type="file"
+                  accept="image/*"
+                  ref="file_avatar"
+                >
+                <span class="file-cta">
+                  <span class="file-label">
+                    {{ teamImageUrl === kAvatarPlaceholder  ?  'Upload Team Avatar' : 'Change Avatar' }}
+                  </span>
                 </span>
-              </span>
-            </label>
+              </label>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="field"> 
-      <label class="label">Team Name</label>
-      <div class="control">
-        <input class="input" type="text" v-model="clone.name" required>
-      </div>
-    </div>
-    <div class="field"> 
-      <label class="label">Where is your team located?</label>
-      <div class="control">
-        <input class="input" type="text" placeholder="City" v-model="clone.city" required>
-      </div>
-    </div>
-    <div class="field">
-      <div class="control">
-        <input class="input" type="text" placeholder="State/Province/Region" v-model="clone.region" required>
-      </div>
-    </div>
-    <div class="field">
-      <div class="control">
-        <input class="input" type="text" placeholder="Country" v-model="clone.country" required>
-      </div>
-    </div>
-    <hr/>
-    <div class="field"> 
-      <h2 class="title is-4">Team Members</h2>
-      <h3 class="subtitle">Add the discord handle of your team members</h3>
-      <div
-        v-for="(val, i) in clone.discord_usernames"
-        :key="i"
-        class="field is-grouped"
-      >
+      <div class="field"> 
+        <label class="label">Team Name</label>
         <div class="control">
-          <input
-          class="input" 
-          type="text" 
-          v-model="clone.discord_usernames[i]" /> 
+          <input class="input" type="text" v-model="clone.name" required>
         </div>
-        <div 
-          class="control"
+      </div>
+      <div class="field"> 
+        <label class="label">Where is your team located?</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="City" v-model="clone.city" required>
+        </div>
+      </div>
+      <div class="field">
+        <div class="control">
+          <input class="input" type="text" placeholder="State/Province/Region" v-model="clone.region" required>
+        </div>
+      </div>
+      <div class="field">
+        <div class="control">
+          <input class="input" type="text" placeholder="Country" v-model="clone.country" required>
+        </div>
+      </div>
+      <hr/>
+      <div class="field"> 
+        <h2 class="title is-4">Team Members</h2>
+        <h3 class="subtitle">Add the discord handle of your team members</h3>
+        <div
+          v-for="(val, i) in clone.discord_usernames"
+          :key="i"
+          class="field is-grouped"
         >
-          <button
-            @click.prevent="clone.discord_usernames.splice(i, 1)" 
-            class="button is-text">
-            remove
+          <div class="control">
+            <input
+            class="input" 
+            type="text" 
+            v-model="clone.discord_usernames[i]" /> 
+          </div>
+          <div 
+            class="control"
+          >
+            <button
+              @click.prevent="clone.discord_usernames.splice(i, 1)" 
+              class="button is-text">
+              remove
+            </button>
+          </div>
+        </div>
+        <a
+          @click="clone.discord_usernames.push('')"
+          class="button is-primary m-2"
+        >
+          + Add More Members
+        </a>
+      </div>
+      <hr/>
+      <div class="field"> 
+        <label class="label">About your team</label>
+        <text-editor 
+          :value="clone.about"
+          :placeholder="kAboutPlacehoder"
+          @text-change="(change) => clone.about = change" 
+        />
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button @click="$emit('cancel')" class="button is-outlined">
+            Cancel
+          </button>
+        </div>
+        <div class="control">
+          <button 
+            type="submit"
+            class="button is-primary"
+            :class="{'is-loading': isSaving}"
+            :disabled="disableSubmit"
+          >
+            Save Team
           </button>
         </div>
       </div>
-      <a
-        @click="clone.discord_usernames.push('')"
-        class="button is-primary m-2"
+    </form>
+  </section>
+  <section class="section mt-6">
+    <div
+      v-if="team.id"
+      class="p-4 has-background-light"
+    >
+      <h3 class="title is-5">Delete Team</h3>
+      <p><strong>Warning:</strong> Deleting a Team is irreversible. All associated data will also be deleted.</p>
+      <button
+        class="button is-danger mt-3"
+        @click="confirmDelete(team)"
       >
-        + Add More Members
-      </a>
+        Delete Team
+      </button>
     </div>
-    <hr/>
-    <div class="field"> 
-      <label class="label">About your team</label>
-      <text-editor 
-        :value="clone.about"
-        :placeholder="kAboutPlacehoder"
-        @text-change="(change) => clone.about = change" 
-      />
-    </div>
-    <div class="field is-grouped is-grouped-right">
-      <div class="control">
-        <button @click="$emit('cancel')" class="button is-outlined">
-          Cancel
-        </button>
-      </div>
-      <div class="control">
-        <button 
-          type="submit"
-          class="button is-primary"
-          :class="{'is-loading': isSaving}"
-          :disabled="disableSubmit"
-        >
-          Save Team
-        </button>
-      </div>
-    </div>
-  </form>
+  </section>
 </template>
 
 <script lang="ts">
@@ -128,6 +145,7 @@ import { LogLevel, TeamRole } from '@/enums'
 import { mapStores } from 'pinia'
 import { useTeamsStore } from '@/store/teams'
 import { useFlashStore } from '@/store/flash'
+import { useModalStore } from '@/store/modal'
 import { TEAM_AVATAR_PLACEHOLDER } from '@/consts'
 import log from '@/services/logger'
 
@@ -167,7 +185,7 @@ export default defineComponent({
     }
   },
   computed: {
-    ...mapStores(useTeamsStore, useFlashStore),
+    ...mapStores(useTeamsStore, useFlashStore, useModalStore),
     teamImageUrl():string { 
       let result = this.avatarPreviewUrl || TEAM_AVATAR_PLACEHOLDER
       if (this.clone.avatar && this.clone.avatar.url) {
@@ -232,6 +250,31 @@ export default defineComponent({
       else {
         this.flashStore.$patch({ message:`The Avatar file size cannot exceed ${AVATAR_MAX_FILE_SIZE}kb.`, level: LogLevel.error })
       }
+    },
+    confirmDelete(team:Team) {
+      this.modalStore.options = {
+        title: 'Delete Team?',
+        component: 'Confirm',
+        meta: {
+          message: `Are you sure you want to delete ${team.name}?`,
+          confirm: () => { this.deleteTeam(team) },
+          confirmLabel: 'Delete',
+          cancelLabel: 'Cancel'
+        }
+      }
+    },
+    deleteTeam(team:Team) {
+      this.teamsStore.deleteTeam(team)
+        .then(result => {
+            this.$emit("team-deleted")
+          })
+          .catch(error => {
+            this.flashStore.$patch({ message: 'Error deleting team. Please try again.', level: LogLevel.error })
+            log.error(MODULE_ID, error)
+          })
+          .finally(() => {
+            this.isSaving = false
+          })
     }
   }
 })
