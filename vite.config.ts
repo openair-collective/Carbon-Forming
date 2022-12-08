64bab90e16899d58949f4237120270c9a86311c7
Chris Couchoud <dev@southriverstudios.com>
2022-12-08 12:05:18 -0500

    Moved bulma sass utils import to vite css preproccessor

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,13 @@ export default defineConfig(({command, mode }) => {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "bulma/sass/utilities/_all.sass";`
+        }
+      },
+    },
     build: {
       minify: mode !== 'development',
       // sourcemap: mode === 'development' // still an issue https://github.com/vitejs/vite-plugin-vue/issues/35
