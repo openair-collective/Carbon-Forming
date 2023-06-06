bc8f9faa76fcb8d27bd981cd2eb2c72a290b650e
Chris Couchoud <dev@southriverstudios.com>
2023-06-06 11:49:26 -0400

    added About page content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,26 @@
 <template>
   <section class="section">
     <article>
-      <header>
-        <h1 class="title is-1">About</h1>
+      <header class="mb-2">
+        <h1 class="title is-2">What Is Carbon Crowd All About?</h1>
       </header>
+      <div class="content">
+        <p><strong><em>Peer-production</em></strong> is a way of making things that relies on distributed, self-organizing, online communities of individuals voluntarily contributing their ideas, expertise and time to develop and continuously improve products and services. Contemporary peer-production was enabled by the internet and first established in practice with the open source software movement decades ago. But it has gained influence across a broad spectrum of fields in the years since, including hardware development, media and content creation, and scientific research.</p>
+        <p>CarbonCrowd is a new platform that aims to apply the potential of peer-production to the critical need of accelerating progress in the area of carbon dioxide removal, or CDR. It was conceived and launched by the OpenAir Collective, a volunteer CDR action group founded in 2019 with active volunteer members all over the world.</p>
+        <p>Our objective is to create a space that enables individuals and small groups anywhere on earth to directly participate in and help realize continuous improvements to open source CDR technologies that are being developed within the OpenAir community. This can take the form of sharing knowledge, expertise, time, energy and influence to advance the evolution of shared projects.</p>
+        <p>At the core of Carbon Crowd is a passion and commitment to building things and solving problems in the real world. In practice, this entails:</p>
+        <ul>
+          <li>
+            <p><strong><em>Bi-monthly design-build relays.</em></strong> Every two months, new versions of OpenAir sponsored CDR innovations are built by member teams to solve particular challenges or optimize functional or utilization aspects of those innovations. During any build cycle, a single member team is responsible for building the new version. But they are directly supported by a growing network of community members who provide knowledge and expertise, suggestions, and even financial support along the way to help the build team achieve their design goal. When a team completes its build, another team is selected to lead the next two-month build process, setting a new optimization goal to achieve within this period. This process can continue indefinitely, leading to continuous evolution and improvement.</p>
+          </li>
+          <li>
+            <p><strong><em>Everything is open source.</em></strong> OpenAir is committed to open source development models that enable anyone, anywhere to download design documents and build their own versions of community-sponsored CDR innovations. We believe that this radically transparent approach has the unique potential to accelerate CDR technology evolution and take it in new and creative directions that conventional markets alone may not be able to replicate or outperform.</p>
+          </li>
+          <li>
+            <p><strong><em>Everyone is welcome.</em></strong> Professional designers, scientists and engineers are a core part of our creative community. But everyone is invited to jump in and play a role. That means amateurs, students and complete novices to anything related to design and technology. We believe that the more diverse the spectrum of our member experiences, the more likely we are to escape restrictive disciplinary silos and discover creative breakthrough approaches.</p>
+          </li>
+        </ul>
+      </div>
     </article>
   </section>
 </template>
